8848c680ad5919639ec90f7540ade6994550c16d
Ashutosh Singh <146580975+Abhiashu2026@users.noreply.github.com>
2024-05-01 07:03:04 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) 
+// it depends on you to use semi colon or not
+
+let name = "ashu"
+let age  = 18 // number is 2 to the power 52 or bigint can be used
+let isLoggedIn = false //boolean
+let state; ////undefined 
+
+//null => standalone value, its an object
+
+//symbol=> uniqueness
+//objet
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
